1dbd92a3dd1093e6ee80dd95cd6aeb8590d1b388
wangshuaiyu <414095706@qq.com>
2020-07-03 21:54:51 +0800

    处理iOS 端删除群组回调消息的格式错误问题

--- ios/Classes/message/OtherMessageNode.swift
+++ ios/Classes/message/OtherMessageNode.swift
@@ -14,8 +14,22 @@ public class OtherMessageNode : AbstractMessageNode{
     
     override func analysis(elem: TIMElem) -> AbstractMessageEntity {
         let entity = OtherMessageEntity();
-        entity.params = "{\"note\":\"IOS暂不支持该节点!\"}";
-        entity.type = "\(type(of:elem))";
+        if(elem.isMember(of: TIMGroupSystemElem.self)){
+            var e: TIMGroupSystemElem
+            e = elem as! TIMGroupSystemElem;
+            print(e.type);
+            if(e.type == TIM_GROUP_SYSTEM_TYPE.DELETE_GROUP_TYPE){
+                entity.params = "{\"subtype\":\"TIM_GROUP_SYSTEM_TYPE.DELETE_GROUP_TYPE\"}";
+                entity.type = "GroupSystem";
+            }else{
+                entity.params = "{\"note\":\"IOS暂不支持该节点!\"}";
+                entity.type = "\(type(of:elem))";
+            }
+            
+        }else{
+            entity.params = "{\"note\":\"IOS暂不支持该节点!\"}";
+            entity.type = "\(type(of:elem))";
+        }
         return entity;
     }
 }
